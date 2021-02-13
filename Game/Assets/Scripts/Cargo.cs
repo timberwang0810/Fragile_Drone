7076962350193650968c039f6053019b20597b69
jiwooy <jiwyoo629@gmail.com>
2021-02-13 10:19:29 -0500

    conveyer belt

---
+++ Game/Assets/Scripts/Cargo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cargo : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "ConveyerBelt" && transform.parent == null)
+        {
+            ConveyerBelt cb = collision.gameObject.GetComponent<ConveyerBelt>();
+            Vector3 newPos = new Vector3(transform.position.x + (cb.direction * Time.deltaTime * cb.speed), transform.position.y, transform.position.z);
+            transform.position = newPos;
+        }
+    }
+}
