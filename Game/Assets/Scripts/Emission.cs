f0c581f504b1ea63abf1c001e4ea7f09e2af091a
timberwang0810 <zilinwan@andrew.cmu.edu>
2021-02-13 00:06:24 -0500

    added emission script

---
+++ Game/Assets/Scripts/Emission.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Emission : MonoBehaviour
+{
+    // Emitted Substance (e.g. fire, steam)
+    public GameObject substance;
+
+    // Time of emission breaks
+    public float timeBetweenEmission;
+
+    // Time of emission
+    public float timeOfEmission;
+
+    // Start delay -- gives all emissions different start times
+    public float timeStartDelay;
+
+    private void Start()
+    {
+        substance.SetActive(false);
+        StartEmission();
+    }
+
+    private void StartEmission()
+    {
+        StartCoroutine(StartDelay());
+    }
+
+    private IEnumerator StartDelay()
+    {
+        yield return new WaitForSeconds(timeStartDelay);
+        StartCoroutine(StartEmitLoop());
+    }
+
+    private IEnumerator StartEmitLoop()
+    {
+        while (true)
+        {
+            substance.SetActive(true);
+            yield return new WaitForSeconds(timeOfEmission);
+            substance.SetActive(false);
+            yield return new WaitForSeconds(timeBetweenEmission);
+        }
+    }
+
+    /**
+     * Alternate Version below
+     **/
+
+    //private void StartEmitLoop()
+    //{
+    //    StartCoroutine(Emit());
+    //}
+
+    //private IEnumerator Emit()
+    //{ 
+    //    substance.SetActive(true);
+    //    yield return new WaitForSeconds(timeOfEmission);
+    //    substance.SetActive(false);
+    //    yield return new WaitForSeconds(timeBetweenEmission);
+    //    StartEmitLoop();
+    //}
+}
